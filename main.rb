463c7c65b9ec03d01c741c43cff5754726dbc210
daisuke.matsushima <haru56utauhito@gmail.com>
2021-11-04 13:03:57 +0000

    main.rb ver2.0

--- main.rb
+++ main.rb
@@ -2,9 +2,15 @@ require './human'
 
 # インスタンスの作成
 tanaka = Human.new("田中太郎", 25, "電車")
-# suzuki = Human.new
-# sato   = Human.new
+suzuki = Human.new("鈴木次郎", 30, "野球")
+sato   = Human.new("佐藤花子", 20, "映画")
 
 # メソッドを実行
 tanaka.say
 tanaka.think
+
+suzuki.say
+suzuki.think
+
+sato.say
+sato.think
